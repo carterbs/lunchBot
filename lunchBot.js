eead95d3eb8802bd2486a3ae00aa7f392f3b14e8
Brad Carter <carterbs89@gmail.com>
2016-08-07 20:09:32 -0400

    Removed console logs and added some neat ascii art

--- lunchBot.js
+++ lunchBot.js
@@ -1,40 +1,21 @@
-/*~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-           ______     ______     ______   __  __     __     ______
-          /\  == \   /\  __ \   /\__  _\ /\ \/ /    /\ \   /\__  _\
-          \ \  __<   \ \ \/\ \  \/_/\ \/ \ \  _"-.  \ \ \  \/_/\ \/
-           \ \_____\  \ \_____\    \ \_\  \ \_\ \_\  \ \_\    \ \_\
-            \/_____/   \/_____/     \/_/   \/_/\/_/   \/_/     \/_/
-This is a sample Slack bot built with Botkit.
-This bot demonstrates many of the core features of Botkit:
-* Connect to Slack using the real time API
-* Receive messages based on "spoken" patterns
-* Reply to messages
-* Use the conversation system to ask questions
-* Use the built in storage system to store and retrieve information
-  for a user.
-# RUN THE BOT:
-  Get a Bot token from Slack:
-    -> http://my.slack.com/services/new/bot
-  Run your bot from the command line:
-    token=<MY TOKEN> node slack_bot.js
-# USE THE BOT:
-  Find your bot inside Slack to send it a direct message.
-  Say: "Hello"
-  The bot will reply "Hello!"
-  Say: "who are you?"
-  The bot will tell you its name, where it is running, and for how long.
-  Say: "Call me <nickname>"
-  Tell the bot your nickname. Now you are friends.
-  Say: "who am I?"
-  The bot will tell you your nickname, if it knows one for you.
-  Say: "shutdown"
-  The bot will ask if you are sure, and then shut itself down.
-  Make sure to invite your bot into other channels using /invite @<my bot>!
-# EXTEND THE BOT:
-  Botkit has many features for building cool and useful bots!
-  Read all about it here:
-    -> http://howdy.ai/botkit
-~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~*/
+/***
+ *       ,--,                                                                                      ,----, 
+ *    ,---.'|                          ,--.                   ,--,               ,----..         ,/   .`| 
+ *    |   | :                        ,--.'|  ,----..        ,--.'|    ,---,.    /   /   \      ,`   .'  : 
+ *    :   : |            ,--,    ,--,:  : | /   /   \    ,--,  | :  ,'  .'  \  /   .     :   ;    ;     / 
+ *    |   ' :          ,'_ /| ,`--.'`|  ' :|   :     :,---.'|  : ',---.' .' | .   /   ;.  \.'___,/    ,'  
+ *    ;   ; '     .--. |  | : |   :  :  | |.   |  ;. /|   | : _' ||   |  |: |.   ;   /  ` ;|    :     |   
+ *    '   | |__ ,'_ /| :  . | :   |   \ | :.   ; /--` :   : |.'  |:   :  :  /;   |  ; \ ; |;    |.';  ;   
+ *    |   | :.'||  ' | |  . . |   : '  '; |;   | ;    |   ' '  ; ::   |    ; |   :  | ; | '`----'  |  |   
+ *    '   :    ;|  | ' |  | | '   ' ;.    ;|   : |    '   |  .'. ||   :     \.   |  ' ' ' :    '   :  ;   
+ *    |   |  ./ :  | | :  ' ; |   | | \   |.   | '___ |   | :  | '|   |   . |'   ;  \; /  |    |   |  '   
+ *    ;   : ;   |  ; ' |  | ' '   : |  ; .''   ; : .'|'   : |  : ;'   :  '; | \   \  ',  /     '   :  |   
+ *    |   ,/    :  | : ;  ; | |   | '`--'  '   | '/  :|   | '  ,/ |   |  | ;   ;   :    /      ;   |.'    
+ *    '---'     '  :  `--'   \'   : |      |   :    / ;   : ;--'  |   :   /     \   \ .'       '---'      
+ *              :  ,      .-./;   |.'       \   \ .'  |   ,/      |   | ,'       `---`                    
+ *               `--`----'    '---'          `---`    '---'       `----'                                  
+ *                                                                                                        
+ */
 
 if (!process.env.token) {
     console.log('Error: Specify token in environment');
@@ -73,8 +54,6 @@ var today = new Date();
 //initialization function
 function startBot(){
     controller.storage.users.get(bot.identity.id, function(err, data){
-        console.log(err, data);
-        console.log(bot.identity.id);
         if(!data){
             data = {data:{}};
             var defaultData = {lunchOptions: CONFIG.defaultRestaurants, thisWeeksWinners:[]};
@@ -93,7 +72,6 @@ function startBot(){
 }
 
 controller.hears(['lunchbot, list all restaurants'], 'direct_message, mention', function(bot,message){   
-    console.log(message); 
     controller.storage.users.get(bot.identity.id, function(err, data){
         restaurants = data.restaurants;
         var names=[];
@@ -162,9 +140,7 @@ function createPoll(){
     bot.say({channel:CONFIG.pollChannel, text: ":three: " + restaurants[2]});
     bot.say({channel:CONFIG.pollChannel, text: ":four: " + restaurants[3]});
     bot.say({channel:CONFIG.pollChannel, text: ":five: " + restaurants[4]}); 
-    //Count votes 75 minutes after creating the poll.
-    setTallyTimer();
-            
+    setTallyTimer();            
 };
 
 function setTallyTimer(){
@@ -173,7 +149,6 @@ function setTallyTimer(){
     var rightNow = new Date();
     if(tallyTime > rightNow){
      setTimeout(function(){
-        console.log(bot.lunchTally, bot.todaysOptions);
             var winner = false,
                 numVotes = 0,
                 tie = false;
@@ -184,7 +159,6 @@ function setTallyTimer(){
                     numVotes = bot.lunchTally[key];                    
                     continue;                     
                 }
-                console.log(bot.lunchTally[key])
                 if (bot.lunchTally[key] > numVotes) {
                     winner = bot.todaysOptions[i];
                     numVotes = bot.lunchTally[key];                    
@@ -209,7 +183,6 @@ function setTallyTimer(){
 controller.on('reaction_added', function(bot, message){
     //if someone's reacting to a message the bot has added.
     if(message.item.channel == CONFIG.pollChannel && message.item_user == bot.identity.id){
-        console.log(message);
         //if i need to reprimand people for voting multiple times...code below will return user object.
         //bot.api.users.info({user:message.user})
         if(bot.lunchVoters.indexOf(message.user) == -1 ) {
@@ -241,37 +214,3 @@ controller.on('reaction_added', function(bot, message){
         }
     }
 });
-
-
-
-
-
-controller.hears(['uptime', 'identify yourself', 'who are you', 'what is your name'],
-    'direct_message,direct_mention,mention', function(bot, message) {
-
-        var hostname = os.hostname();
-        var uptime = formatUptime(process.uptime());
-
-        bot.reply(message,
-            ':robot_face: I am a bot named <@' + bot.identity.name +
-             '>. I have been running for ' + uptime + ' on ' + hostname + '.');
-
-    });
-
-function formatUptime(uptime) {
-    var unit = 'second';
-    if (uptime > 60) {
-        uptime = uptime / 60;
-        unit = 'minute';
-    }
-    if (uptime > 60) {
-        uptime = uptime / 60;
-        unit = 'hour';
-    }
-    if (uptime != 1) {
-        unit = unit + 's';
-    }
-
-    uptime = uptime + ' ' + unit;
-    return uptime;
-}
