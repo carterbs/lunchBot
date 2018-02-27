44b42713af6dee30ae7053dbbc1583e6ae4c5bba
Brad Carter <carterbs89@gmail.com>
2018-02-26 19:29:17 -0500

    Comments

--- classes/RestaurantFetcher.js
+++ classes/RestaurantFetcher.js
@@ -2,10 +2,15 @@
 const async = require("async");
 const GoogleSpreadsheet = require("google-spreadsheet");
 const support = require("../SupportFunctions");
+const CONFIG = require("../config.json");
 
+/**
+ * Little class to manage retrieval of a list of restaurants from a google spreadsheet. Also handles
+ * the selection of restauarants used by LunchBot.
+*/
 class RestaurantFetcher {
 	constructor() {
-		this.spreadsheetKey = "1R6lYIdGdvni6rEKaaBRykqF0WsT56DuuiANKjP6ZDEU";
+		this.spreadsheetKey = CONFIG.google_sheets_key;
 		this.doc = new GoogleSpreadsheet(this.spreadsheetKey);
 		this.sheet = null;
 		this.getSheet = this.getSheet.bind(this);
@@ -13,27 +18,40 @@ class RestaurantFetcher {
 		this.selectRestaurants = this.selectRestaurants.bind(this);
 		this.fetchRestaurants = this.fetchRestaurants.bind(this);
 	}
+	/**
+	 * Goes and fetches the worksheet from google docs.
+	 * @param {Function} cb
+	 */
 	fetchRestaurants(cb) {
 		return async.series([this.getSheet, this.getDataFromSheet], err => {
-			console.log("finished");
 			if (cb) {
 				return cb(err, this.restaurants);
 			}
 		});
 	}
+	/**
+	 * Uses the fetched restaurantList randomly selects restaurants.
+	 * @param {Array} previousWinners
+	 */
 	selectRestaurants(previousWinners = []) {
 		return support.selectRestaurants(this.restaurants, previousWinners);
 	}
+	/**
+	 * Handles the call to google docs and stores the sheet as an object on the class.
+	 * @param {Function} done
+	 */
 	getSheet(done) {
 		console.log("Retrieving doc...");
 		this.doc.getInfo((err, info) => {
 			console.log(`Loaded doc: ${info.title} by ${info.author.email}`);
 			this.sheet = info.worksheets[0];
-			const sheet = this.sheet;
-			console.log(`sheet 1: ${sheet.title} ${sheet.rowCount}x${sheet.colCount}`);
 			done(err);
 		});
 	}
+	/**
+	 * Parses the datasheet, creating the list of restaurants.
+	 * @param {*} done
+	 */
 	getDataFromSheet(done) {
 		this.sheet.getRows({
 			offset: 1,
